ecafa85dc6f36ed47273b6cc1bce1d9b0a8be2ff
Ana Beatriz de Oliveira Assis <107081846+oabeatriz@users.noreply.github.com>
2023-03-17 14:39:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# Faça um programa que peça ao usuário seu nome e idade, e imprima uma mensagem de boas-vindas com essas informações.
+nome = input("Digite seu nome: ")
+print(f"Bem vindo(a), {nome}!")
+
+
+
+
+# Faça um programa que peça ao usuário um número e imprima o dobro desse número.
+
+numero = float(input("Digite um número: "))
+dobro = numero * 2
+print(f"O dobro de {numero} é: {dobro} ")
+
+
+# Faça um programa que peça ao usuário uma palavra e imprima essa palavra em maiúsculas
+
+palavra = (input("Digite uma palavra: "))
+maiuscula = palavra.upper()
+print(f"A palavra em maiúsculo é: {maiuscula}")
+
+# Faça um programa que peça ao usuário o raio de um círculo e calcule a área desse círculo.
+
+raio = float(input("Digite o raio de um círculo: "))
+area = 3.14 * raio * raio
+print(f"A área deste círculo é: {area}")
+
+
+#Faça um programa que peça ao usuário um número e calcule a raiz quadrada desse número.
+
+num = float(input("Digite um número: "))
+raiz = float(num) ** 0.5
+print(f"A raiz quadrade deste número é: {raiz}")
+
+
+
+
+
